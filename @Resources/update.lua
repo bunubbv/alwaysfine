c313393d9cdb9da8d54faab90cbe52fb9a004ab7
Doiram <me@doiram.com>
2023-08-12 02:55:42 +0900

    fix: typo in update script

--- @Resources/update.lua
+++ @Resources/update.lua
@@ -1,6 +1,6 @@
 function updateExecute(p, hash)
     path = SKIN:MakePathAbsolute(p)
-    command = string.format('powershell -command "cd \'%s\';cd ../;wget https://***************************** -OutFile update.zip";$pubHash = \'%s\';$fileHash = Get-FileHash update.zip -Algorithm md5;if($fileHash.Hash -eq $pubHash){;expand-archive -path \'update.zip\';RD AlwaysFine -Recurse;mv update AlwaysFine -Recurse;RD update -Recurse;}else{;exit;}', path, hash)
+    command = string.format('powershell -command "cd \'%s\';cd ../;cd ../;wget https://*****************************\'&\'download=1 -OutFile update.zip;$pubHash = \'%s\';$fileHash = Get-FileHash update.zip -Algorithm md5;if($fileHash.Hash -eq $pubHash){;expand-archive -Force -path \'update.zip\';RD AlwaysFine -Recurse;mv update AlwaysFine;RD update.zip -Force;}else{;exit;}"', path, hash)
 
     os.execute(command)
 end
