8d11c93bd7d0d4eb655c73a95c2f54c292eed6d4
Doiram <me@doiram.com>
2023-08-12 19:08:21 +0900

    fix: bug during update and renew
    docs: add update history

--- @Resources/update.lua
+++ @Resources/update.lua
@@ -1,6 +1,6 @@
 function updateExecute(p, hash, lo)
     path = SKIN:MakePathAbsolute(p)
-    command = string.format('powershell -command "cd \'%s\';wget %s\'&\'download=1 -OutFile update.zip;$pubHash = \'%s\';$fileHash = Get-FileHash update.zip -Algorithm md5;if($fileHash.Hash -eq $pubHash){;expand-archive -Force -path \'update.zip\';cd ../;cd ../;mv AlwaysFine/@Resources/update AlwaysFine -Recurse;}else{;exit;}"', path, lo, hash)
+    command = string.format('powershell -command "cd \'%s\';wget %s\'&\'download=1 -OutFile AlwaysFine_Update.zip;$pubHash = \'%s\';$fileHash = Get-FileHash AlwaysFine_Update.zip -Algorithm md5;if($fileHash.Hash -eq $pubHash){;expand-archive -Force -path \'AlwaysFine_Update.zip\';cd ../;cd ../;cp \'AlwaysFine/@Resources/AlwaysFine_Update\' -Destination \'AlwaysFine_Update\' -Recurse -Force;RD AlwaysFine -Recurse;mv AlwaysFine_Update AlwaysFine;}else{;exit;}"', path, lo, hash)
 
     os.execute(command)
 end
