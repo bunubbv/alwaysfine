afed1c3e391c973d5d97d42207dae60b62190d18
Doiram <me@doiram.com>
2023-08-12 14:48:17 +0900

    fix: bug that could delete the wrong directory during update
    refactor: update structure and code security

--- @Resources/update.lua
+++ @Resources/update.lua
@@ -1,6 +1,6 @@
-function updateExecute(p, hash)
+function updateExecute(p, hash, lo)
     path = SKIN:MakePathAbsolute(p)
-    command = string.format('powershell -command "cd \'%s\';cd ../;cd ../;wget https://*****************************\'&\'download=1 -OutFile update.zip;$pubHash = \'%s\';$fileHash = Get-FileHash update.zip -Algorithm md5;if($fileHash.Hash -eq $pubHash){;expand-archive -Force -path \'update.zip\';RD AlwaysFine -Recurse;mv update AlwaysFine;RD update.zip -Force;}else{;exit;}"', path, hash)
+    command = string.format('powershell -command "cd \'%s\';wget %s\'&\'download=1 -OutFile update.zip;$pubHash = \'%s\';$fileHash = Get-FileHash update.zip -Algorithm md5;if($fileHash.Hash -eq $pubHash){;expand-archive -Force -path \'update.zip\';cd ../;cd ../;mv AlwaysFine/@Resources/update AlwaysFine -Recurse;}else{;exit;}"', path, lo, hash)
 
     os.execute(command)
 end
