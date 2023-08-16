a32303e1a1729a65ba150965f638bb093054d2bc
Doiram <me@doiram.com>
2023-08-16 21:35:33 +0900

    fix: bugs during update and in setting page (#5, #6)

--- @Resources/update.lua
+++
@@ -1,6 +0,0 @@
-function updateExecute(p, hash, lo)
-    path = SKIN:MakePathAbsolute(p)
-    command = string.format('powershell -command "cd \'%s\';wget %s\'&\'download=1 -OutFile AlwaysFine_Update.zip;$pubHash = \'%s\';$fileHash = Get-FileHash AlwaysFine_Update.zip -Algorithm md5;if($fileHash.Hash -eq $pubHash){;expand-archive -Force -path \'AlwaysFine_Update.zip\';cd ../;cd ../;cp \'AlwaysFine/@Resources/AlwaysFine_Update\' -Destination \'AlwaysFine_Update\' -Recurse -Force;RD AlwaysFine -Recurse;mv AlwaysFine_Update AlwaysFine;}else{;exit;}"', path, lo, hash)
-
-    os.execute(command)
-end
